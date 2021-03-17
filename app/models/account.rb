20a8d5c1c59b1535eb5019d0162f5f8ae6befb54
Chandra Lama <iamme24cl@gmail.com>
2021-03-17 16:37:19 -0700

    implemented a update_balance method on account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,8 @@
 class Account < ApplicationRecord
   has_many :transactions
   validates :name, presence: true
+
+  def update_balance
+    self.balance = Account.joins(:transactions).where("transactions.account_id = ?", self.id).sum(:amount)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_221931) do
+ActiveRecord::Schema.define(version: 2021_03_17_232926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
-    t.float "balance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "balance", default: 0.0
   end
 
   create_table "transactions", force: :cascade do |t|
