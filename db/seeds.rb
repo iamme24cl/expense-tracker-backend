85923c0ac3477f2962dd75946a55a0315920b81d
Chandra Lama <iamme24cl@gmail.com>
2021-03-17 16:01:30 -0700

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Account.destroy_all
+Transaction.destroy_all
+
+
