a0943e83395d557601a1fa977f7bdbfd8461877f
Chandra Lama <iamme24cl@gmail.com>
2021-03-17 18:55:04 -0700

    implemented namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :accounts, only: [:index, :show] do
+        resources :transactions, only: [:index, :create]
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
