a8439d73b0a28ec3d5f912aa285ecb794892024d
Chandra Lama <iamme24cl@gmail.com>
2021-03-19 18:44:35 -0700

    adjusted update_balance method for Account model to update total_income and total_expense

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,8 +5,11 @@ class Account < ApplicationRecord
   def update_balance(transaction)
     if transaction[:kind].downcase == "income" 
       self.balance += transaction[:amount]
+      self.total_income += transaction[:amount]
     else
       self.balance -= transaction[:amount]
+      self.total_expense += transaction[:amount]
     end
   end
+
 end
