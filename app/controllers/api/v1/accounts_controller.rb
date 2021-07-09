5fe783be145158a039eb7b74ac9e2d5a7c939648
Chandra Lama <iamme24cl@gmail.com>
2021-07-09 10:07:48 -0700

    uncommented code in AccountsController

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::AccountsController < ApplicationController
 
-  # def index
-  #   accounts = Account.all
+  def index
+    accounts = Account.all
 
-  #   render json: accounts
-  # end
+    render json: accounts
+  end
 
-  # def show
-  #   account = Account.find_by(id: params[:id])
+  def show
+    account = Account.find_by(id: params[:id])
 
-  #   render json: account
-  # end
+    render json: account
+  end
 end
